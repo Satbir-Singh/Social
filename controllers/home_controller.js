a92f2121e36b8745aa1bce3fe96aef56cb10f106
Das Satbir Singh <dsatbir1@gmail.com>
2022-01-25 22:33:59 +0530

    Authentication using passport js 1

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,30 +1,9 @@
-module.exports.home = function(req,res){
-    
-    //return res.end('<h1>Express is up for codeial</h1>');
-
+module.exports.home = function(req, res){
     console.log(req.cookies);
-    // res.cookie('user_id',25);
-
+    // res.cookie('user_id', 25);
     return res.render('home', {
         title: "Home"
     });
-
-
 }
 
-
-module.exports.signIn = function(req,res){
-
-    return res.render('sign_in',{
-        title: "Sign In Page"
-        
-    });
-}
-
-module.exports.signUp = function(req,res){
-    return res.render('sign_up',{
-        title: "Sign Up Page"
-      
-    });
-}
-
+// module.exports.actionName = function(req, res){}
