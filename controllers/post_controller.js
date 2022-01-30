adace763686ef8ac28adad454d824d232edca834
Das Satbir Singh <dsatbir1@gmail.com>
2022-01-30 23:36:52 +0530

    to add and delete post using ajax

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -2,23 +2,34 @@ const Post = require('../models/post');
 const Comment = require('../models/comment')
 
 
-// function to create async await
+// function to create a post 
 module.exports.create = async function(req,res){
 
     try {
 
-       await Post.create({
+       let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         
         });
 
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post Created!"
+            });
+        }
+
+        req.flash('success', 'Post Published!');
         return res.redirect('back');
         
     } catch (err) {
-        
-        console.log('error in posting message'); 
-        return;
+
+        req.flash('error', err);
+        // console.log('error in posting message'); 
+        return res.redirect('back');
     }
 
    
@@ -39,17 +50,30 @@ module.exports.destroy = async function(req, res){
 
                     //functin only take one arguments becz delete will not return anything
                     await Comment.deleteMany({post: req.params.id});
+
+                    if(req.xhr){
+                        return res.status(200).json({
+                            data: {
+                                post_id: req.params.id
+                            },
+                            message: "Post deleted!"
+                        });
+                    }
+
+                    req.flash('success', 'Post and associated comments deleted!');
                     return res.redirect('back');
 
                 }else{
 
+                    req.flash('error','You cannot delete this post!');
                     return res.redirect('back');
                 }
             
         } catch (err) {
 
-            console.log('Error', err); 
-            return;
+            req.flash('error', err);
+            // console.log('Error', err); 
+            return res.redirect('back');
             
         }   
 
