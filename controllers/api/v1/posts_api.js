fdff0f5a04d3aa90291811640ee783ada23230fc
Das Satbir Singh <dsatbir1@gmail.com>
2022-02-02 16:04:55 +0530

    JWT Authentication for api and using postman as a frontend

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,64 @@
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req,res){
+    
+    
+    let posts = await Post.find({}).populate('user')
+        .sort('-createdAt')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+        });
+    
+    return res.json(200, {
+        message: "List of posts",
+        posts: posts
+    })
+}
+
+
+module.exports.destroy = async function(req, res){
+
+
+
+    try {
+            let post = await Post.findById(req.params.id);
+            // .id means converting the object id into the string
+            if(post.user == req.user.id) {
+
+                post.remove();
+
+                //functin only take one arguments becz delete will not return anything
+                await Comment.deleteMany({post: req.params.id});
+
+               
+
+                
+                return res.json(200, {
+                    message: "Post and associated comments deleted successfully!"
+                });
+
+            }else{
+
+                return res.json(401, {
+
+                    message: "You cannot delete this post!"
+                });
+            }
+        
+    } catch (err) {
+
+
+        console.log('*****', err);
+        return res.json(500, {
+
+            message: "Internal Server Error"
+        });
+        
+
+    }   
+
+}
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const User = require('./models/user');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 // const MongoStore = require('connect-mongo');
 
 const sassMiddleware = require('node-sass-middleware');
@@ -32,6 +33,7 @@ app.use(cookieParser());
 app.use(express.static('./assets'));
 //make the upload path available to the browser
 app.use('/uploads' , express.static(__dirname + '/uploads'));
+
 
 app.use(expressLayouts);
 app.set('layout extractStyles', true);
@@ -88,6 +90,7 @@ app.use(flash());
 app.use(customMware.setFlash);
 
 app.use('/', require('./routes'));
+
 
 
 
